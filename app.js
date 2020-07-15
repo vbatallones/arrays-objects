670fbce6e463bfc7c88b5c5d514d3113f41a260d
Levin Batallones <levinbatallones@Batallones.local>
2020-07-15 14:24:02 -0700

    Add while loops

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ console.log(favoriteNumber);
 
 let teams = ['Rockets', 'Lakers', 'Warriors', 'Celtics', 'Blazzers', 'clippers', 'Nets'];
 console.log(teams);
+
+var num = 10;
+var otherNum = 1;
+while (otherNum < num) {
+    console.log(otherNum);
+    otherNum+=1
+}
