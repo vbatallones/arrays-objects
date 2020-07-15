8dcdc168ead33de7651095c783f1870bbbb82127
Levin Batallones <levinbatallones@Batallones.local>
2020-07-15 14:03:55 -0700

    Add favorite teams

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = "Levin";
+console.log(name);
+
+let myLocation = "Seattle";
+console.log(myLocation);
+
+let favoriteNumber = 27;
+console.log(favoriteNumber);
+
+let teams = ['Rockets', 'Lakers', 'Warriors', 'Celtics', 'Blazzers', 'clippers', 'Nets'];
+console.log(teams);
